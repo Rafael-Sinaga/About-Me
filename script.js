6c49e77cdb1e51dcbbf14c915ee947429883f9af
Rafael Sinaga <rafaelmangapulrolassinaga@gmail.com>
2026-04-16 21:51:10 +0700

    minor update

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ document.querySelectorAll('.slider-wrapper').forEach(wrapper => {
     slider.scrollBy({ left: -300, behavior: 'smooth' });
   });
 });
+
+/* SCROLL REVEAL */
+const reveals = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.15
+});
+
+reveals.forEach(reveal => {
+  observer.observe(reveal);
+});
